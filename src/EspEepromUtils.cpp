1232e1bb564413144c0c5ef2a4f2b4b3d9e65949
Christophe Dirac <cdirac@me.com>
2019-06-15 13:19:00 +0200

    Initial commit

---
+++ src/EspEepromUtils.cpp
@@ -0,0 +1,52 @@
+#include <Arduino.h>
+#include "EspEepromUtils.h"
+
+#include <ESP.h>
+
+void EspEepromUtils::wipeEEPROM() {
+	for (uint16_t i = 0 ; i < 4096; i++) {
+		EEPROM.write(i, 0x00);
+	}
+	EEPROM.commit();
+}
+
+void EspEepromUtils::writeEEPROM(uint16_t addr, unsigned long data) {
+	writeEEPROM(addr, String(data));
+}
+
+void EspEepromUtils::writeEEPROM(uint16_t addr, long data) {
+	writeEEPROM(addr, String(data));
+}
+
+void EspEepromUtils::writeEEPROM(uint16_t addr, double data) {
+	writeEEPROM(addr, String(data));
+}
+
+void EspEepromUtils::writeEEPROM(uint16_t addr, uint8_t data) {
+	writeEEPROM(addr, String(data));
+}
+
+void EspEepromUtils::writeEEPROM(uint16_t addr, String data) {
+	uint8_t size = data.length();
+	for (uint8_t i = 0; i < size; i++) {
+		EEPROM.write(addr + i, data.charAt(i));
+	}
+	EEPROM.write(addr + size, 0);
+	EEPROM.commit();
+}
+
+String EspEepromUtils::readEEPROM(uint16_t addr) {
+	String result = "";
+	uint8_t count = 0;
+	while (true) {
+		char byte = EEPROM.read(addr + count);
+		if (byte == '\0') {
+			break;
+		}
+		else {
+			result.concat(byte);
+		}
+		count++;
+	}
+	return result;
+}
---
+++ src/EspEepromUtils.h
@@ -0,0 +1,17 @@
+#ifndef EspEepromUtils_h
+#define EspEepromUtils_h
+
+#include <WString.h>
+
+class EspEepromUtils {
+  public:
+  static void wipeEEPROM();
+  static void writeEEPROM(uint16_t, unsigned long);
+  static void writeEEPROM(uint16_t, long);
+  static void writeEEPROM(uint16_t, double);
+  static void writeEEPROM(uint16_t, uint8_t);
+  static void writeEEPROM(uint16_t, String);
+  static String readEEPROM(uint16_t);
+};
+
+#endif
